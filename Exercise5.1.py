a39b2a549182aa9ba8344acb9020bbcd90faddf9
S-j-654 <shakeynaglory023@icloud.com>
2026-03-23 19:25:08 +0100

    Add files via upload

---
+++ Exercise5.1.py
@@ -0,0 +1,12 @@
+
+for i in range (1, 21):
+    if i % 3 == 0 and i % 5 == 0:
+        print("Data Science")
+    elif i % 3 == 0:
+        print("Data")
+    elif i % 5 == 0:
+        print("Science")
+    else:
+        print(i)
+
+
